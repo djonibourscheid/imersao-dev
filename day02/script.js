91cbba3ddacedbf3de0b692114712ce97419b771
Djoni Bourscheid <djonibourscheid00@gmail.com>
2022-09-06 19:59:48 -0300

    Dia 02

---
+++ day02/script.js
@@ -0,0 +1,19 @@
+let quotationUSD;
+
+const valueUSDfetch = fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+  .then(resp => resp.json())
+  .then(resp => {
+    quotationUSD = resp.USDBRL.high;
+	})
+  
+
+
+function converter() {
+  let inputNumber = parseFloat(document.getElementById("valor").value);
+  inputNumber < 0 ? inputNumber = 0 : inputNumber;
+
+  const account = (inputNumber / quotationUSD).toFixed(2);
+
+  const responseEl = document.getElementById("valorConvertido");
+  responseEl.innerText = `O valor em dolar é $${account}`;
+}
