72d09ebb0e74b607bd8b2015477ff18327dc6227
Djoni Bourscheid <djonibourscheid00@gmail.com>
2022-09-20 19:05:02 -0300

    Dia 09 e 10/Página inicial: Responsividade
    Página da aula e a página principal eram iguais. Apaguei a da aula

--- day09and10/script.js
+++ day09and10/script.js
@@ -1,7 +1,35 @@
+const buttonEl = document.getElementById("toggle-theme");
+
 function toggleTheme() {
   document.body.classList.toggle("dark");
 
-  const buttonEl = document.getElementById("toggle-theme");
   buttonEl.classList.toggle("light");
   buttonEl.classList.toggle("dark");
 }
+
+window.addEventListener("resize", () => {
+  const windowWidth = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+  const windowHeight = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+
+  let marginX;
+  let marginY;
+
+  if (windowWidth > 1800) {
+    marginX = (windowWidth - 1440) / 2;
+    buttonEl.style.right = `calc(${marginX}px + 1rem)`;
+  } else {
+    buttonEl.style.right = "";
+  }
+
+  if (windowHeight > 800) {
+    marginY = (windowHeight - 655.5) / 2;
+    buttonEl.style.top = `calc(${marginY}px + 1rem)`;
+  } else {
+    buttonEl.style.top = "";
+  }
+});
