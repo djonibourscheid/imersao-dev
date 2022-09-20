83403dd495b8bf737e0642decf6936a10add7baf
Djoni Bourscheid <djonibourscheid00@gmail.com>
2022-09-20 19:25:59 -0300

    Salvando o modo de cor no LocalStorage

--- day09and10/script.js
+++ day09and10/script.js
@@ -1,10 +1,41 @@
+let themeStorage = localStorage.getItem("menuTheme") || "light";
+
 const buttonEl = document.getElementById("toggle-theme");
+
+if (themeStorage === "light") {
+  document.body.classList.remove("dark");
+
+  buttonEl.classList.add("light");
+  buttonEl.classList.remove("dark");
+
+} else if (themeStorage === "dark") {
+  document.body.classList.add("dark");
+
+  buttonEl.classList.remove("light");
+  buttonEl.classList.add("dark");
+}
 
 function toggleTheme() {
   document.body.classList.toggle("dark");
 
   buttonEl.classList.toggle("light");
   buttonEl.classList.toggle("dark");
+
+  switch (themeStorage) {
+    case "light":
+      themeStorage = "dark";
+      break;
+
+    case "dark":
+      themeStorage = "light";
+      break;
+
+    default:
+      themeStorage = "light";
+      break;
+  }
+
+  localStorage.setItem("menuTheme", themeStorage);
 }
 
 window.addEventListener("resize", () => {
